44e7cc45ee68ea4e0e2e8a7cc62cbf4dfac04ced
Chen Weiling <weiling.chen@hmgics.com>
2022-08-03 16:09:09 +0800

    reduce memomry usage

--- extract_features_weiling.py
+++ extract_features_weiling.py
@@ -9,40 +9,35 @@ from resnet import i3_res50
 
 def run(i3d, frequency, temppath, batch_size, sample_mode, last_segment):
     dataloader = create_dataloader(temppath + "../", frequency, 0)
-    i3d.cuda()
-    i3d.train(False)  # Set model to evaluate mode
     frames = []
-
-    for i, (inputs, _) in enumerate(dataloader):
+    full_features = [[] for i in range(10)]
+    for i, (inputs, _) in enumerate(dataloader):  # i is the index of the frame
         if i == len(dataloader) - 1:
             if inputs.shape[0] < frequency:
                 inputs = torch.cat([inputs, inputs[-1].repeat(frequency - inputs.shape[0], 1, 1, 1, 1)], dim=0)
-        frames.append(inputs)  # 16,10,3,224,224
+        frames.append(inputs)
+        if (i + 1) % batch_size == 0 or i == len(dataloader) - 1:  # 每320个feed一次model or 数据全部取出
+            print(i)
+            frames_batch = torch.cat(frames, dim=0)
+            frames_batch = frames_batch.view(-1, frequency, 10, 3, 224, 224)  # [20, 16, 10, 3, 224, 224]
+            frames_batch = frames_batch.permute(2, 0, 3, 1, 4, 5)  # [10, 20, 16, 3, 224, 224]
+            for j in range(10):
+                with torch.no_grad():
+                    b_data = frames_batch[j].cuda()
+                    inp = {'frames': b_data}  # bsx3x16x224x224
+                    # print(i, j)
+                    outputs = i3d(inp)
+                full_features[j].append(outputs.data.cpu().numpy())
+            frames = []
 
-    print("frame no.", len(frames))
-    full_features = [[] for i in range(10)]
-    for i in range(0, len(frames), batch_size):
-        print(i)
-        start = i
-        end = i + batch_size if i + batch_size < len(frames) else len(frames)
-        frames_batch = torch.cat(frames[start:end], dim=0)
-        frames_batch = frames_batch.view(-1, frequency, 10, 3, 224, 224)  # [20, 16, 10, 3, 224, 224]
-        frames_batch = frames_batch.permute(2, 0, 3, 1, 4, 5)  # [10, 20, 16, 3, 224, 224]
-        for i in range(10):
-            with torch.no_grad():
-                b_data = Variable(frames_batch[i].cuda()).float()
-                inp = {'frames': b_data}  # bsx3x16x224x224
-                outputs = i3d(inp)
-            full_features[i].append(outputs.data.cpu().numpy())
-
-    # {list: 10, 57}, each shape = [20,2048, 1,1 1], 20 is batch size, 57 is no. of batches
+    # full_features: {list: 10, batch_cnt}, each shape = [20,2048, 1,1 1], 20 is batch size, batch_cnt is no. of batches
     full_features = [np.concatenate(i, axis=0) for i in
-                     full_features]  # {list: 10}, each shape = [1140,2048, 1,1,1]
+                     full_features]  # {list: 10}, each shape = [batch_cnt * bs, 2048, 1,1,1]
     full_features = [np.expand_dims(i, axis=0) for i in
-                     full_features]   # {list: 10}, each shape = [1,1140,2048, 1,1,1]
-    full_features = np.concatenate(full_features, axis=0)  # [10,1140,2048, 1,1,1]
-    full_features = full_features[:, :, :, 0, 0, 0]  # [10,1140,2048]
-    full_features = np.array(full_features).transpose([1, 0, 2])  # [1140,10,2048]
+                     full_features]  # {list: 10}, each shape = [1,batch_cnt * bs,2048, 1,1,1]
+    full_features = np.concatenate(full_features, axis=0)  # [10,batch_cnt * bs,2048, 1,1,1]
+    full_features = full_features[:, :, :, 0, 0, 0]  # [10,batch_cnt * bs,2048]
+    full_features = np.array(full_features).transpose([1, 0, 2])  # [batch_cnt * bs,10,2048]
     print(full_features.shape)
     return full_features
 
